155e2d312e918a823e71d2f2a5aa001eb0b25806
natan-alex <natancarmo321@gmail.com>
2022-06-21 21:40:16 -0300

    add tab between distances in matrix

--- common/DistanceMatrix.java
+++ common/DistanceMatrix.java
@@ -35,10 +35,10 @@ public class DistanceMatrix {
 
     public void show() {
         for (int i = 0; i < matrix.length; i++) {
-            System.out.print("[  ");
+            System.out.print("[\t");
 
             for (int j = 0; j < matrix.length; j++) {
-                System.out.print(matrix[i][j] + "  ");
+                System.out.print(matrix[i][j] + "\t");
             }
 
             System.out.println("]");
